c3200b07b01ec0c8e39b271e11a9fe4f0b6db1f5
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2023-10-12 19:26:10 -0300

    "Adicionando rotas do carrinho e dos alimentos"

--- pln-backend/main.py
+++ pln-backend/main.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 def hello_page():
     return "<p>First page of the application</p>"
 
-@app.route("/appname/query")
-def query():
+@app.route("/recipemaker/query")
+def send_query():
+    pass
+
+@app.route("/recipemaker/fetch/cart")
+def fetch_cart():
+    pass
+
+@app.route("/recipemaker/fetch/foods")
+def fetch_foods():
     pass
 
 if __name__ == '__main__':
