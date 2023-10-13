340c817d5c6017d4ad2e860ce5497f98b86b3538
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2023-10-13 14:29:14 -0300

    gpt api access granted

---
+++ pln-backend/api_access.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key = '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'
+
+messages = [ {"role": "system", "content":  
+              "You are a intelligent assistant."} ] 
+while True: 
+    message = input("User : ") 
+    if message: 
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=messages 
+        ) 
+      
+    reply = chat.choices[0].message.content 
+    print(f"ChatGPT: {reply}") 
+    messages.append({"role": "assistant", "content": reply})
+
