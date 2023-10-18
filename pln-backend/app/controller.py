c3a6c6f15b53fc3bb0be2360b9282dc2182b6638
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2023-10-18 20:56:03 -0300

    Removing unused files

--- pln-backend/app/controller.py
+++ pln-backend/app/controller.py
@@ -1,10 +1,9 @@
 import openai
 from flask import Flask, request, jsonify
-from app.recipe_maker_app import RecipeMakerApp
+
 from api_credentials import API_KEY
 
 app = Flask(__name__)
-recipe_app = RecipeMakerApp()
 openai.api_key = API_KEY
 
 # Sends query to chat gpt and returns its response
@@ -12,7 +11,6 @@ openai.api_key = API_KEY
 def send_query():
     json = request.get_json()
     message = json['query']
-    print(message)
 
     messages = [ {"role": "system", "content":  
               "You are a intelligent assistant."} ]
@@ -27,16 +25,6 @@ def send_query():
     reply = chat.choices[0].message.content
     messages.append({"role": "assistant", "content": reply})
     return jsonify(reply)
-
-# Fetches cart data
-@app.route("/recipemaker/fetch/cart")
-def fetch_cart():
-    pass
-
-# Fetches all available foods
-@app.route("/recipemaker/fetch/foods")
-def fetch_foods():
-    pass
 
 if __name__ == '__main__':
     app.run()
