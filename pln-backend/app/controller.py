5c60452831c356c0d94834e4336e8badd168cec2
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2023-10-22 10:13:58 -0300

    Adicionando refatoração na composição da query

--- pln-backend/app/controller.py
+++ pln-backend/app/controller.py
@@ -1,13 +1,11 @@
 import openai
 from flask import Flask, request, jsonify
-from flask_cors import CORS,cross_origin
-
+from flask_cors import CORS, cross_origin
 from api_credentials import API_KEY
 
 app = Flask(__name__)
 CORS(app)
 openai.api_key = API_KEY
-from flask_cors import CORS
 
 cors = CORS(app, resources={
     r"/api/*": {
@@ -23,25 +21,8 @@ cors = CORS(app, resources={
 def send_query():
     json = request.get_json()
     json_data = json["data"]
-    foods_list = [food for food in json_data['foods']]
 
-    foods_string = ""
-    for food in foods_list:
-        foods_string += f"{food},"
-
-    options = json_data['options']
-
-    message = "Act as a chef and suggest me a recipe according to the following recommendations:"
-    for op in options.keys():
-        sentences = ""
-        if options[op] and op == "difficult":
-            sentences += f"\nI want a recipe of the difficulty {options[op]}."
-        elif options[op] and op == "nutrition":
-            sentences += f"\nI want a recipe that goes according to the nutrition value: {options[op]}"
-        elif options[op] and op =="drink":
-            sentences += f"\nI want a drink {options[op]} to go with the recipe, please sepparate the drink recommendation from the recipe and tell me why you think that is a great drink recommendation."
-
-    message += f"\nNow, suggest me the best possible recipe, according to what I described knowing that I have the following ingredients: {foods_string}. If you feel like there's any ingredient missing, please tell me which ones I'm missing for the recipe you have."
+    message = compose_query(json_data)
 
     messages = [ {"role": "system", "content":  
               "You are a intelligent assistant."} ]
@@ -52,20 +33,54 @@ def send_query():
     chat = openai.ChatCompletion.create( 
             model="gpt-3.5-turbo", messages=messages
         )
-    print("-"*90)
+
     try:
         response = chat.choices[0].message.content
-        print(f"O tipo da resposta é: {type(response)}")
+
         messages.append({"role": "assistant", "content": response})
         response_dict = {"recipe" : response}
-        print(f"respondi {response_dict}")
+
         return jsonify(response_dict)
+    
     except Exception as e:
-        print("Error!")
-        print("*" * 90)
         error = {"error message" : f"{str(e)}"}
-        print(error)
         return jsonify(error)
+
+def compose_query(json: dict[str: any]):
+
+    # Compose a string with all foods
+    foods_list = [food for food in json['foods']]
+    foods_string = ", ".join(foods_list)
+
+    options = json['options']
+
+    sentences = ""
+    professional = "chef"
+    if options["difficult"]:
+        sentences += f"\n-I want a {options['difficult']} level recipe in terms of difficulty."
+    
+    if options["nutrition"]:
+        professional = "nutritionist"
+        sentences += f"\n-I want a recipe that goes according to the nutrition value: {options['nutrition']}"
+
+    if options["drink"]:
+        sentences += f"""\n-I want a {options['drink']} drink to go with the recipe,
+                            please sepparate the drink recommendation from the recipe
+                            and tell me why you think that is a great drink to go with
+                            the suggested recipe."""
+
+    message = f"Act as a {professional} and suggest me a recipe according to the following recommendations:"
+    message += sentences
+
+    message += f"""\nNow, suggest me the best possible recipe,
+                according to what I described knowing that I 
+                have the following ingredients: {foods_string}.
+                If you feel like the ingredients are not enough
+                to make a recipe, give me the closest recipe 
+                that matches my ingredients and describe me which
+                ones I'm missing for that certain recipe you have."""
+
+    return message
 
 if __name__ == '__main__':
     app.run()
